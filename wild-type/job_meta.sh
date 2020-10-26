b6ee25a155827c69180acca44bde17bc0dbf0206
HuixiaLu <49809354+HuixiaLu@users.noreply.github.com>
2020-10-26 18:06:32 +0100

    Add files via upload

--- wild-type/job_meta.sh
+++ wild-type/job_meta.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-#$ -S /bin/bash
-#$ -q md.q
-#$ -N Chol-wt
-#$ -pe openmpi 16
-#$ -cwd
-cd /home/huixia/CHOL-KRAS/
 
-gmx mdrun -nt 16 -deffnm 1600ns-metadynamics -cpi 1600ns-metadynamics.cpt -noappend -v -plumed plumed-1.dat
+gmx mdrun -nt 16 -deffnm 800ns-metadynamics -v -plumed plumed-1.dat
 
